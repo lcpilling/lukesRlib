0464f471914cb30186c01aba9a4802c9cf2064cb
Luke C. Pilling <luke.pilling@gmail.com>
2023-01-27 14:43:34 +0000

    Add more p/z functions

--- R/p_values.R
+++ R/p_values.R
@@ -6,7 +6,7 @@
 #'
 #' @author The internet (anon)
 #'
-#' @name extreme_p
+#' @name get_extreme_p
 #'
 #' @param z a z (or t) statistic
 #'
@@ -18,7 +18,7 @@
 #' @export
 #'
 
-get_extreme_p <- function(z) {
+get_extreme_p = function(z) {
 	if (!is.numeric(z))  stop("z needs to be numeric")
 	log_pvalue   = log(2) + pnorm(abs(z), lower.tail = FALSE, log.p = TRUE)
 	log10_pvalue = log_pvalue/log(10) ## from natural log to log10
@@ -50,9 +50,62 @@ get_extreme_p <- function(z) {
 #' @export
 #'
 
-get_neglog10_p <- function(z, n) {
+get_neglog10_p = function(z, n) {
 	if (!is.numeric(z))  stop("z needs to be numeric")
 	if (!is.numeric(n))  stop("n needs to be numeric")
 	neglog10_p=-1*(pt(abs(z),df=n,lower.tail=F,log.p=T) + log(2))/log(10)
 	neglog10_p
 }
+
+
+#' Get p-value from Z-statistic
+#'
+#' @description Return p-value for a z (or t) statistic
+#'
+#' @return Returns a p-value (numeric)
+#'
+#' @author The internet (anon)
+#'
+#' @name get_p
+#'
+#' @param z a z (or t) statistic
+#'
+#' @examples
+#' z = 10
+#' get_p(z)
+#' #>  [1] 1.523971e-23
+#'
+#' @export
+#'
+get_p = function(z) {
+	if (!is.numeric(z))  stop("z needs to be numeric")
+	p = 2*pnorm(-abs(z))
+	p
+}
+
+
+#' Get Z-statistic from p-value
+#'
+#' @description Return a z-statistic from a given p-value
+#'
+#' @return Returns a Z-statistic (numeric)
+#'
+#' @author The internet (anon)
+#'
+#' @name get_z
+#'
+#' @param p a p-value
+#'
+#' @examples
+#' p = 1e-10
+#' get_z(p)
+#' #>  [1] 6.466951
+#'
+#' @export
+#'
+get_z = function(p) {
+	if (!is.numeric(p))  stop("p needs to be numeric")
+	z = abs(qnorm(p/2,0,1))
+	z
+}
+
